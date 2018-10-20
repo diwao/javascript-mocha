5e1e6ba34d07eff01e6aef39c7fb61f3670bde7e
diwao <nakapom@gmail.com>
2018-10-20 16:37:12 +0900

    test: 三角測量のテストを追加

--- test/imaginary_number_test.js
+++ test/imaginary_number_test.js
@@ -3,6 +3,17 @@ const assert = require('assert');
 const ImaginaryNumber = require('../lib/ImaginaryNumber');
 
 describe('実部と虚部を与えて虚数を生成する', () => {
-  const sut = new ImaginaryNumber(4, 2);
-  assert(sut.toString() === '4 + 2i');
+  context('実部に4、虚部に2を与えた場合', () => {
+    it('文字列表現は"4 + 2i"', () => {
+      const sut = new ImaginaryNumber(4, 2);
+      assert(sut.toString() === '4 + 2i');
+    })
+  });
+
+  context('実部に5、虚部に3を与えた場合', () => {
+    it('文字列表現は"5 + 3i"', () => {
+      const sut = new ImaginaryNumber(5, 3);
+      assert(sut.toString() === '5 + 3i');
+    })
+  });
 });
