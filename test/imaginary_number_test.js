6fb78d79a0e182b35f4b4136de68b402b2ad2bb7
diwao <nakapom@gmail.com>
2018-10-20 16:41:43 +0900

    test: 虚部が負の数である場合の虚数の文字列表現のテストを追加

--- test/imaginary_number_test.js
+++ test/imaginary_number_test.js
@@ -16,4 +16,11 @@ describe('実部と虚部を与えて虚数を生成する', () => {
       assert(sut.toString() === '5 + 3i');
     })
   });
+
+  context('実部に4、虚部に-2を与えた場合', () => {
+    it('文字列表現は"4 - 2i"', () => {
+      const sut = new ImaginaryNumber(4, -2);
+      assert(sut.toString() === '4 - 2i');
+    })
+  });
 });
