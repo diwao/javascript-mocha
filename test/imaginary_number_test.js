c3c459cb5022592b166dcb898f277ae6289bb7fc
diwao <nakapom@gmail.com>
2018-10-20 16:34:07 +0900

    test: 虚数を生成するテストを追加

--- test/imaginary_number_test.js
+++ test/imaginary_number_test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const ImaginaryNumber = require('../lib/ImaginaryNumber');
 
-describe('虚数', () => {
+describe('実部と虚部を与えて虚数を生成する', () => {
   const sut = new ImaginaryNumber(4, 2);
-  assert(sut);
+  assert(sut.toString() === '4 + 2i');
 });
